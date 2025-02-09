8d813c1977428ac2a54600d876770ce4807d344f
David Kalmuk <correo@correo.com>
2025-02-09 17:23:15 +0100

    Creamos la estructura basica del register y login

--- src/app.py
+++ src/app.py
@@ -9,6 +9,16 @@ def index():
 
     return render_template('home.html')
 
+@app.route('/login',methods=['POST','GET'])
+def login():
+
+    return render_template('login.html')
+
+@app.route('/register',methods=['POST','GET'])
+def register():
+
+    return render_template('register.html')
+
 
 if __name__=='__main__':
     app.config.from_object(config['development'])
